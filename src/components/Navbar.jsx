560b124c8e0d684eed054f375d50db625a44e77e
Tanmay Dongare <tvnmvy@gmail.com>
2025-03-04 12:30:09 +0530

    minor ui fixes...

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,22 +3,35 @@ import '../App.css'
 
 function Navbar() {
   return (
-    <div className="fixed top-0 left-0 w-full z-50 bg-white border-b backdrop-blur-lg bg-opacity-80">
-    <div className="mx-auto max-w-7xl px-6 sm:px-6 lg:px-8 ">
-        <div className="relative flex h-16 justify-between">
-            <div className="flex flex-1 items-stretch justify-start">
-                <a className="flex flex-shrink-0 items-center" href="#">
-                    <img className="block h-12 w-auto" src="https://www.svgrepo.com/show/501888/donut.svg" />
-                </a>
-            </div>
-            <div className="flex-shrink-0 flex px-2 py-3 items-center space-x-8">
-                <a className="text-gray-700 hover:text-indigo-700 text-sm font-medium" href="#">Login</a>
-                <a className="text-gray-800 bg-indigo-100 hover:bg-indigo-200 inline-flex items-center justify-center px-3 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm "
-                    href="#">Sign up
-                </a>
-            </div>
-        </div>
+    <div className="navbar bg-base-100 shadow-sm">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li><a>Homepage</a></li>
+        <li><a>Portfolio</a></li>
+        <li><a>About</a></li>
+      </ul>
     </div>
+  </div>
+  <div className="navbar-center">
+    <a className="btn btn-ghost text-xl">BlogCellar</a>
+  </div>
+  <div className="navbar-end">
+    <button className="btn btn-ghost btn-circle">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /> </svg>
+    </button>
+    <button className="btn btn-ghost btn-circle">
+      <div className="indicator">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /> </svg>
+        <span className="badge badge-xs badge-primary indicator-item"></span>
+      </div>
+    </button>
+  </div>
 </div>
   )
 }
