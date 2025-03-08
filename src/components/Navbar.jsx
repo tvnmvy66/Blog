812fbad407cc58bbd17e5d3e2832ffa0c025e5a2
Tanmay Dongare <tvnmvy@gmail.com>
2025-03-09 02:08:40 +0530

    fixes.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -90,7 +90,7 @@ const Navbar = () => {
               role="menu"
             >
               <li className="flex gap-2 items-center p-2">
-                {!localStorage.getItem("user-info") ? (<div>
+                {localStorage.getItem("user-info") ? (<div>
                   <div className="avatar">
                   <div className="h-10 w-10 rounded-full">
                     <img
